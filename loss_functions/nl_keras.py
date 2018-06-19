1e7b3c8d2122b87c7de86d0f6b629724c80317e4
John Martinsson <john.martinsson@gmail.com>
2018-06-19 15:47:32 +0200

    Fix the nll loss function

--- loss_functions/nl_keras.py
+++ loss_functions/nl_keras.py
@@ -4,13 +4,10 @@ def load():
     return tf_negative_log_normal_density_loss
 
 def tf_negative_log_normal_density_loss(y_true, y_pred):
-    n = y_pred.shape[1]
-    y_mean = y_pred[:,n//2:]
-    y_var  = y_pred[:,:n//2]
+    y_var = y_pred[:,:1]
+    y_mean = y_pred[:,1:]
     y_std  = tf.sqrt(tf.abs(y_var)) # TODO: This is a hack!
 
     dist = tf.distributions.Normal(loc=y_mean, scale=y_std)
     probs = dist.prob(y_true)
     return tf.reduce_mean(-tf.log(tf.keras.backend.epsilon() + probs)) # TODO: This is a hack!
-
-
