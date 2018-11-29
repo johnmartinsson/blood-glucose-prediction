54ca3e910846de3008470b7aa8211af4739261a4
John Martinsson <john.martinsson@gmail.com>
2018-11-29 13:21:07 +0100

    Adds SEG metric and plot

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import scipy
+from scipy.interpolate import interp2d
 
 # numpy metrics
 def surveillance_error(targets, predictions):
@@ -9,12 +10,10 @@ def surveillance_error(targets, predictions):
 
     xs = np.linspace(0, 600, data.shape[0])
     ys = np.linspace(0, 600, data.shape[1])
-    f = scipy.interpolate.interp2d(xs, ys, np.transpose(data))
+    f = interp2d(xs, ys, np.transpose(data))
 
-    ma = np.max(data)
-    scores = [f(t, p) for (t, p) in zip(targets, predictions)]
-    # print(scores)
-    return np.sum(scores)/(ma*len(targets))
+    scores = np.concatenate([f(t, p) for (t, p) in zip(targets, predictions)])
+    return np.mean(scores), np.std(scores)
 
 def root_mean_squared_error(targets, predictions):
     return np.sqrt(np.mean(np.power(targets-predictions, 2)))
