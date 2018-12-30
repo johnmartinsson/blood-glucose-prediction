30b6f8a238f5e8a1e304cf1683d2da607c88b75a
Ubuntu <ubuntu@rise-train.novalocal>
2018-12-30 21:28:20 +0000

    Adds training and evaluation scripts for final experiments

---
+++ evaluate_final_experiments.sh
@@ -0,0 +1,20 @@
+#python run.py -f experiments/559_final_experiment.yaml -m evaluate
+#python run.py -f experiments/591_final_experiment.yaml -m evaluate
+#python run.py -f experiments/570_final_experiment.yaml -m evaluate
+#python run.py -f experiments/588_final_experiment.yaml -m evaluate
+#python run.py -f experiments/575_final_experiment.yaml -m evaluate
+#python run.py -f experiments/563_final_experiment.yaml -m evaluate
+
+#python run.py -f experiments/559_final_evaluation.yaml -m evaluate
+#python run.py -f experiments/591_final_evaluation.yaml -m evaluate
+#python run.py -f experiments/570_final_evaluation.yaml -m evaluate
+#python run.py -f experiments/588_final_evaluation.yaml -m evaluate
+#python run.py -f experiments/575_final_evaluation.yaml -m evaluate
+#python run.py -f experiments/563_final_evaluation.yaml -m evaluate
+
+python run.py -f experiments/559_all_final_evaluation.yaml -m evaluate
+python run.py -f experiments/591_all_final_evaluation.yaml -m evaluate
+python run.py -f experiments/570_all_final_evaluation.yaml -m evaluate
+python run.py -f experiments/588_all_final_evaluation.yaml -m evaluate
+python run.py -f experiments/575_all_final_evaluation.yaml -m evaluate
+python run.py -f experiments/563_all_final_evaluation.yaml -m evaluate
---
+++ run_final_experiments.sh
@@ -0,0 +1,7 @@
+python run.py -f experiments/all_final_experiment.yaml -m train
+python run.py -f experiments/559_final_experiment.yaml -m train
+python run.py -f experiments/591_final_experiment.yaml -m train
+python run.py -f experiments/570_final_experiment.yaml -m train
+python run.py -f experiments/588_final_experiment.yaml -m train
+python run.py -f experiments/575_final_experiment.yaml -m train
+python run.py -f experiments/563_final_experiment.yaml -m train
