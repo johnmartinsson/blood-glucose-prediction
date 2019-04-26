a86f2395d8eb449377a0c53822480bed6d829bbc
John Martinsson <john.martinsson@gmail.com>
2019-04-26 11:14:36 +0200

    Updates to latest (minimal) hyperparameter search plots.

--- plot_parameter_search.py
+++ plot_parameter_search.py
@@ -10,12 +10,13 @@ sns.set()
 def main():
     artifacts_path = sys.argv[1]
     d = {}
-    nb_lstm_statess = [8, 32, 96, 128, 256, 512]
     #nb_lstm_statess = [8, 32, 128, 256, 512]
     #nb_past_stepss = [12, 24]
-    nb_past_stepss  = [6, 12, 24, 36]
-    seeds = [0, 1, 2, 3, 4]
+    #seeds = [0, 1, 2, 3, 4]
     #seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    nb_lstm_statess = [8, 32, 128, 256, 512]
+    nb_past_stepss  = [6, 12, 24, 36]
+    seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
     results = np.zeros((len(seeds), len(nb_lstm_statess), len(nb_past_stepss)))
     for i_step, nb_past_steps in enumerate(nb_past_stepss):
         for i_state, nb_lstm_states in enumerate(nb_lstm_statess):
@@ -29,18 +30,46 @@ def main():
 
     # plot rmse score mean and standard deviation with respect to number of LSTM
     # units and number of past steps for five different random seeds
-    for i_step, nb_past_steps in enumerate(nb_past_stepss):
-        xs = nb_lstm_statess
-        ys_mean = np.mean(results, axis=0)[:, i_step]
-        ys_std = np.std(results, axis=0)[:, i_step]
-        plt.plot(xs, ys_mean)
-        plt.fill_between(xs, ys_mean+ys_std, ys_mean-ys_std, alpha=0.5,
-                label='history = {} min'.format(nb_past_steps*5))
+    #for i_step, nb_past_steps in enumerate(nb_past_stepss):
+    xs = nb_lstm_statess
+    i_past_steps = 1
+    ys_mean = np.mean(results, axis=0)[:, i_past_steps]
+    ys_std = np.std(results, axis=0)[:, i_past_steps]
+    plt.plot(xs, ys_mean)
+    plt.fill_between(xs, ys_mean+ys_std, ys_mean-ys_std, alpha=0.5,
+            label='history = {} min'.format(nb_past_stepss[i_past_steps]*5))
 
     plt.ylabel('RMSE')
     plt.xlabel('Number of LSTM units')
     plt.legend(loc = 'upper right')
-    plt.savefig("parameter_search.png", dpi=300)
+    plt.savefig("parameter_search.pdf", dpi=300)
+
+    plt.figure()
+    #for i_step, nb_lstm_states in enumerate(nb_lstm_statess):
+    xs = np.array(nb_past_stepss)*5
+    i_lstm_states = 3
+    ys_mean = np.mean(results, axis=0)[i_lstm_states,:]
+    ys_std = np.std(results, axis=0)[i_lstm_states,:]
+    plt.plot(xs, ys_mean)
+    plt.fill_between(xs, ys_mean+ys_std, ys_mean-ys_std, alpha=0.5,
+            label='LSTM units = {} '.format(nb_lstm_statess[i_lstm_states]))
+
+    plt.ylabel('RMSE')
+    plt.xlabel('History [m]')
+    plt.legend(loc = 'upper right')
+    plt.savefig("parameter_search_reversed.pdf", dpi=300)
+
+
+    plt.figure()
+    xs = np.array(nb_past_stepss)
+    ys = np.mean(results[:,3,:], axis=0)
+    plt.plot(xs*5, ys)
+    plt.ylabel('RMSE')
+    plt.xlabel('History [m]')
+    plt.savefig("parameter_search_history.pdf", dpi=300)
+
+    print(results.flatten()[np.argmin(results)])
+
 
 if __name__ == "__main__":
     main()
