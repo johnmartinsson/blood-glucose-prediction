820d03f4e78efd27dff5ea4f60cb68de64c234fe
John Martinsson <john.martinsson@gmail.com>
2019-04-26 11:16:22 +0200

    Adds script to run plot methods.

---
+++ run_final_plots.sh
@@ -0,0 +1,13 @@
+python run.py -f experiments/559_final_experiment.yaml -m plot_nll
+python run.py -f experiments/591_final_experiment.yaml -m plot_nll
+python run.py -f experiments/570_final_experiment.yaml -m plot_nll
+python run.py -f experiments/588_final_experiment.yaml -m plot_nll
+python run.py -f experiments/575_final_experiment.yaml -m plot_nll
+python run.py -f experiments/563_final_experiment.yaml -m plot_nll
+
+python run.py -f experiments/559_final_experiment.yaml -m plot_seg
+python run.py -f experiments/591_final_experiment.yaml -m plot_seg
+python run.py -f experiments/570_final_experiment.yaml -m plot_seg
+python run.py -f experiments/588_final_experiment.yaml -m plot_seg
+python run.py -f experiments/575_final_experiment.yaml -m plot_seg
+python run.py -f experiments/563_final_experiment.yaml -m plot_seg
